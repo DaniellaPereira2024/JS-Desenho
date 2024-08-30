2ce5a1ac50bff69220aec099ac61bb1a6304e1c7
DaniellaPereira2024 <pereiradaniella376@gmail.com>
2024-08-30 10:05:01 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,25 @@
+function setup() {
+  createCanvas(400, 400);
+}
+  function draw() {
+  background("blue");
+  fill( "black");
+  textSize (100);
+  textAlign (CENTER,CENTER);
+  
+  let maximo = width;
+  let minimo = 0;
+  //let palavra = "Daniella";
+ let quatidade = map (mouseX,0,width,0,palavra.length); 
+    
+  //console.log ("daniella.length");
+    
+ //if(mouseX <50) {
+ // let palavra = "D";
+  // text( palavra, 200,200);   
+ // } else { 
+   
+   //let palavra = "Daniella";
+  // text( palavra, 200,200);  
+ // } 
+  }  
